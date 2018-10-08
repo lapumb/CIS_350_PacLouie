ccb41ed761cc252d0d695eae665d09bb47c6ca95
Blake Lapum <lapumb@mail.gvsu.edu>
2018-10-08 08:57:22 -0400

    linked difficulty page to main screen

--- app/src/main/java/com/example/lapum/paclouie/MainActivity.java
+++ app/src/main/java/com/example/lapum/paclouie/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lapum.paclouie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if(id == R.id.action_difficulty) {
+            Intent intent = new Intent(this, difficulty_page.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
