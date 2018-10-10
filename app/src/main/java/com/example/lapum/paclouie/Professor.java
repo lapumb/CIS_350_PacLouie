42eb1d57c995378731181030499f1cd6636ef61a
Eisenbar <ryan.eisenbarth@gmail.com>
2018-10-10 17:06:17 -0400

    added professor class

---
+++ app/src/main/java/com/example/lapum/paclouie/Professor.java
@@ -0,0 +1,20 @@
+package com.example.lapum.paclouie;
+
+public class Professor {
+
+    int speed;
+    int sprite;
+
+    Professor(){
+        this.speed = 1;
+        //this.sprite = sprite path;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public int getSprite(){
+        return sprite;
+    }
+}
