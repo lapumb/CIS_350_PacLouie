e085b5822b4163175334036672fafd434d73e46f
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2018-10-15 19:27:05 -0400

    Modified Difficulty Page

--- app/src/main/java/com/example/lapum/paclouie/DifficultyActivity.java
+++ app/src/main/java/com/example/lapum/paclouie/DifficultyActivity.java
@@ -1,11 +1,15 @@
 package com.example.lapum.paclouie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class DifficultyActivity extends AppCompatActivity {
 
@@ -13,17 +17,36 @@ public class DifficultyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button btnUnder = findViewById(R.id.btnUnder);
+        btnUnder.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //set number of profs to 1 and open game activity
+                SettingsActivity.num = 1;
+                startActivity(new Intent(DifficultyActivity.this, GameActivity.class));
+
             }
         });
+
+        final Button btnUpper = findViewById(R.id.btnUpper);
+        btnUpper.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //set number of profs to 5 and open game activity
+                SettingsActivity.num = 5;
+                startActivity(new Intent(DifficultyActivity.this, GameActivity.class));
+            }
+        });
+
+        final Button btnGrad = findViewById(R.id.btnGrad);
+        btnGrad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //set number of profs to 9 and open game activity
+                SettingsActivity.num = 9;
+                startActivity(new Intent(DifficultyActivity.this, GameActivity.class));
+            }
+        });
+
+
     }
 
 }
