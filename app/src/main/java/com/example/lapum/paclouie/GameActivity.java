d757472adbe92513e90aa7c942aea85ebc168ad6
Blake Lapum <lapumb@mail.gvsu.edu>
2018-10-14 21:53:04 -0400

    user can specify num of profs via settings page

--- app/src/main/java/com/example/lapum/paclouie/GameActivity.java
+++ app/src/main/java/com/example/lapum/paclouie/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.lapum.paclouie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
@@ -11,11 +12,12 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
-    private int x;
-    private int y;
+    private int x, y;
     private RelativeLayout layout;
     final Random rnd = new Random();
     String str;
+    //public static int SETTINGS_RESULT = 1; //variable to get settings save intent
+    int numProfs = SettingsActivity.num;
 
 
     @Override
@@ -25,31 +27,35 @@ public class GameActivity extends AppCompatActivity {
         layout = (RelativeLayout) findViewById(R.id.gameView);
 
         //initializing images
-        ImageView prof0 = (ImageView) findViewById(R.id.randomTest0);
-        ImageView prof1 = (ImageView) findViewById(R.id.randomTest1);
-        ImageView prof2 = (ImageView) findViewById(R.id.randomTest2);
-        ImageView prof3 = (ImageView) findViewById(R.id.randomTest3);
-        ImageView prof4 = (ImageView) findViewById(R.id.randomTest4);
-        ImageView prof5 = (ImageView) findViewById(R.id.randomTest5);
+        ImageView prof0 = (ImageView) findViewById(R.id.prof0);
+        ImageView prof1 = (ImageView) findViewById(R.id.prof1);
+        ImageView prof2 = (ImageView) findViewById(R.id.prof2);
+        ImageView prof3 = (ImageView) findViewById(R.id.prof3);
+        ImageView prof4 = (ImageView) findViewById(R.id.prof4);
+        ImageView prof5 = (ImageView) findViewById(R.id.prof5);
+        ImageView prof6 = (ImageView) findViewById(R.id.prof6);
+        ImageView prof7 = (ImageView) findViewById(R.id.prof7);
+        ImageView prof8 = (ImageView) findViewById(R.id.prof8);
         ImageView louie = (ImageView) findViewById(R.id.gameLouie);
 
 
+
         //initially setting all profs to not appear (gone)
-        prof0.setVisibility(View.GONE);
         prof1.setVisibility(View.GONE);
         prof2.setVisibility(View.GONE);
         prof3.setVisibility(View.GONE);
         prof4.setVisibility(View.GONE);
         prof5.setVisibility(View.GONE);
+        prof6.setVisibility(View.GONE);
+        prof7.setVisibility(View.GONE);
+        prof8.setVisibility(View.GONE);
+        prof0.setVisibility(View.GONE);
 
 
         //"randomly" generating which (stand-in profs) appear (using a stand in hard number)
-        for(int i = 0; i < 1; i++) {
-            str = "prof" + rnd.nextInt(6);
-            if(str.equals("prof0") && prof0.getVisibility() == View.GONE) {
-                prof0.setVisibility(View.VISIBLE);
-            }
-            else if(str.equals("prof1") && prof1.getVisibility() == View.GONE) {
+        for(int i = 0; i < numProfs; i++) {
+            str = "prof" + rnd.nextInt(9);
+            if(str.equals("prof1") && prof1.getVisibility() == View.GONE) {
                 prof1.setVisibility(View.VISIBLE);
             }
             else if(str.equals("prof2") && prof2.getVisibility() == View.GONE) {
@@ -64,6 +70,18 @@ public class GameActivity extends AppCompatActivity {
             else if(str.equals("prof5") && prof5.getVisibility() == View.GONE) {
                 prof5.setVisibility(View.VISIBLE);
             }
+            else if(str.equals("prof6") && prof6.getVisibility() == View.GONE) {
+                prof6.setVisibility(View.VISIBLE);
+            }
+            else if(str.equals("prof7") && prof7.getVisibility() == View.GONE) {
+                prof7.setVisibility(View.VISIBLE);
+            }
+            else if(str.equals("prof8") && prof8.getVisibility() == View.GONE) {
+                prof8.setVisibility(View.VISIBLE);
+            }
+            else if(str.equals("prof0") && prof0.getVisibility() == View.GONE) {
+                prof0.setVisibility(View.VISIBLE);
+            }
             else {
                 i--;
             }
@@ -74,13 +92,23 @@ public class GameActivity extends AppCompatActivity {
         louie.setOnTouchListener(new ChoiceTouchListener());
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /*if(requestCode == SETTINGS_RESULT) {
+            numProfs = data.getIntExtra("numProfs", 0);
+        } */
+    }
+
+
     private final class ChoiceTouchListener implements View.OnTouchListener {
         public boolean onTouch(View view, MotionEvent event) {
             final int X = (int) event.getRawX();
             final int Y = (int) event.getRawY();
             switch(event.getAction() & MotionEvent.ACTION_MASK) {
                 case MotionEvent.ACTION_DOWN:
-                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
+                            view.getLayoutParams();
                     x = X - params.leftMargin;
                     y = Y - params.topMargin;
                     break;
@@ -95,7 +123,8 @@ public class GameActivity extends AppCompatActivity {
                     break;
 
                 case MotionEvent.ACTION_MOVE:
-                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                            view.getLayoutParams();
                     layoutParams.leftMargin = X - x;
                     layoutParams.topMargin = Y - y;
                     layoutParams.rightMargin = -250;
